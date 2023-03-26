40c7ac34cc92774879a90d0b36817440fb78dc11
Bruno Carvalho <brunocarvalho9810@gmail.com>
2023-03-26 13:08:20 -0300

    Adicionando as primeiras configuracoes kafka consumer

---
+++ src/main/java/com/kafka/consumer/listener/TestListener.java
@@ -0,0 +1,28 @@
+package com.kafka.consumer.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Classe responsável por receber mensagens do topico kafka
+ * @author <a href="https://github.com/brunocarvalho9810/"> Bruno Carvalho </a>
+ */
+@Slf4j
+@Component
+public class TestListener {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TestListener.class);
+
+	/**
+	 * Responsável por receber mensagens do topico "topic-1" e exibir as mensagens por meio de logs
+	 * @author <a href="https://github.com/brunocarvalho9810/"> Bruno Carvalho </a>
+	 */
+	@KafkaListener(topics = "topic-1", groupId = "group-1")
+	public void listen(String message) {
+		logger.info("Thread: {} Messages: {}", Thread.currentThread().getId(), message);
+	}
+}
